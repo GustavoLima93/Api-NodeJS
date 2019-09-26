9299ab979ddd57b7a5f3c7c447c92cc20a4f5543
Gustavo Henrique de Lima Oliveira <gustavolima@Gustavos-MacBook-Pro.local>
2019-09-26 20:13:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tecnologias
+
+- NodeJS
+- MongoDB
+- Sequelize
+
+# API GoBarber
+
+- API construida durante os estudos de NodeJS, utilizando o ORM, banco NoSQL nodemailer, express, entre otras libs.
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '0dac5ec3747836',
+    pass: '5d9b94eecc3da8',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
