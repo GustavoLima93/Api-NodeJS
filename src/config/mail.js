16419576c9480c2c251a471bfbf401fcf3b0b9d1
Gustavo Henrique de Lima Oliveira <gustavolima@Gustavos-MacBook-Pro.local>
2019-09-30 10:52:23 -0300

    go barber concluido

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0dac5ec3747836',
-    pass: '5d9b94eecc3da8',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
